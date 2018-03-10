1929dee0749af0a7860e6d1f17a0c85ac4f91347
Audrey Schmook <audrey.schmook@gmail.com>
2018-03-10 10:29:46 -0600

    moving on to functions

---
+++ index.js
@@ -0,0 +1,21 @@
+
+var beers = 99;
+
+function printVerse(num){
+	
+	var name = 'bottles';
+	if(num === 1) { name = "bottle";}
+
+	console.log(num + " " + name + " of beer on the wall,");
+	console.log(num + " " + name + " of beer,");
+	console.log("You take one down, you pass it around,");
+	num--;
+	console.log(num + " bottles of beer on the wall\n");
+	return num;
+}
+
+//repeat the section 99 times
+while(beers > 0){
+//	console.log("No more bottles of beer on the wall. Time to buy more beer!");
+	beers = printVerse(beers);
+}
